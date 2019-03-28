6c69e4e7aac44aeb20fc2c40a913eef3fddf3b82
dev7k <dev7k@protonmail.com>
2019-03-28 10:01:19 +0100

    added product repository

---
+++ src/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductMicroservice.Model;
+using System.Collections.Generic;
+
+namespace ProductMicroservice.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int productId);
+        void InsertProduct(Product product);
+        void DeleteProduct(int productId);
+        void UpdateProduct(Product productId);
+        void Save();
+    }
+}
