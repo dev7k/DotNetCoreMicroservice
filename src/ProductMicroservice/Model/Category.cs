d498a3c06c37209381453a6f7a3a3699261f8b42
dev7k <dev7k@protonmail.com>
2019-03-28 09:54:41 +0100

    fixed category and product models

--- src/ProductMicroservice/Model/Category.cs
+++ src/ProductMicroservice/Model/Category.cs
@@ -1,9 +1,18 @@
-﻿namespace ProductMicroservice.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductMicroservice.Model
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
     }
 }
--- src/ProductMicroservice/Model/Product.cs
+++ src/ProductMicroservice/Model/Product.cs
@@ -1,11 +1,25 @@
-﻿namespace ProductMicroservice.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductMicroservice.Model
 {
     public class Product
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal Price { get; set; }
+
+        [Required]
         public int CategoryId { get; set; }
     }
 }
